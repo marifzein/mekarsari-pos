40d80588c86f5da555737f0269e0f774750a1a84
M Arif Zein <marifzein@gmail.com>
2026-08-12 21:12:23 +0700

    udpate logo app login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -32,12 +32,14 @@
                 <div class="mb-6">
                     <a href="/" class="flex items-center gap-3 no-underline">
                         {{-- <div class="p-2.5 bg-emerald-500 text-white rounded-xl shadow-md shadow-emerald-200"> --}}
-                        <div class="p-2.5 bg-indigo-500 text-white rounded-xl shadow-md shadow-emerald-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                            </svg>
+                        <div >
+                            
+                            <img 
+                                src="{{ asset('images/tatakas-color.png') }}" 
+                                alt="TataKas Logo" 
+                                class="w-7 h-7 object-contain flex-shrink-0 "
+                            >
                         </div>
-                        {{-- <span class="text-xl font-bold tracking-wider text-gray-800 uppercase">Flow<span class="text-emerald-500">POS</span></span> --}}
                         <span class="text-xl font-bold tracking-wider text-gray-800 uppercase">Tata<span class="text-indigo-500">Kas</span></span>
                     </a>
                 </div>
